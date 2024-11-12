9d32d1fb9dcc5a21d963c8157ec840d5fe0065c3
FranciscoVazquez <franciscovazquez211@gmail.com>
2024-11-12 14:13:00 -0300

    colocando imagenes en proyecto

--- Entrega4TP/js/index.js
+++ Entrega4TP/js/index.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded",()=>{
 //id de seccion    
 let sections = document.querySelectorAll('.scroll-section');
 
+// Seleccionar el logo y el header
+const logo = document.querySelector(".logo-principal-header");
+const header = document.querySelector(".header-container");
+
 //calcular progreso de scroll por seccion
 function calcularScrollProgress(element){
     const rect = element.getBoundingClientRect();
@@ -25,11 +29,25 @@ window.addEventListener("scroll",()=>{
         let x = element.x + (width/2);
         let y = element.y + (height/2);*/
         console.log("Seccion:"+idSection+" "+calcularScrollProgress(e));
+        console.log(window.scrollY);
         
-
-        if(calcularScrollProgress(e)>0 && calcularScrollProgress(e)<1){
-        
+        if(window.scrollY>=125){
+            logo.classList.add('logo-small');
+            header.classList.add('header-container-scroll');
+        }else{
+            logo.classList.remove('logo-small');
+            header.classList.remove('header-container-scroll');
         }
+        if(idSection==="uno"){
+        }
+
+
+
+        /*
+        if(calcularScrollProgress(e)>0 && calcularScrollProgress(e)<1){
+            console.log("estoy en seccion:" + idSection)
+            if
+        }*/
     
     });
 
