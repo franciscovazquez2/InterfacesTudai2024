518bdcf47be8cf0bcecf496e46f156b0eda3c73e
FranciscoVazquez <franciscovazquez211@gmail.com>
2024-11-14 09:05:56 -0300

    add paralax section 1 -logo sin funcionar

--- Entrega4TP/js/index.js
+++ Entrega4TP/js/index.js
@@ -8,6 +8,9 @@ let sections = document.querySelectorAll('.scroll-section');
 // Seleccionar el logo y el header
 const logo = document.getElementById('logo-principal-header');
 const header = document.querySelector('.header-container');
+//seccion paralax 1
+let layers = document.querySelectorAll('.parallax');
+
 
 //calcular progreso de scroll por seccion
 function calcularScrollProgress(element){
@@ -22,30 +25,33 @@ window.addEventListener("scroll",()=>{
     
     sections.forEach((e)=>{
         let idSection = e.id;
-       
-        console.log(window.scrollY);
-        
+               
         if(idSection==="uno"){
             let viewDisplay = calcularScrollProgress(e);
+            console.log(viewDisplay);
             if(window.scrollY>5 && viewDisplay<0.5){
                 let scaleValue = 0.9-(viewDisplay);
-                logo.style.transform=`translateY(-150px) scale(${scaleValue})`;
+                logo.style.transform=`translateY(-35px) scale(${scaleValue})`;
                 header.classList.add('header-container-scroll');
                 
             }if(window.scrollY<=5){
                 logo.style.transform=`scale(${(1)})`;
                 header.classList.remove('header-container-scroll');
-            }  
+            } 
+            
+            layers.forEach((layer)=>{
+                
+                let top =scrollY;
+                let speed = layer.getAttribute('data-speed');
+                let posX = -(top*speed/100);
+                let posY = -(top*speed/100);
+                
+                layer.setAttribute('style','transform:translate3d(0px,'+posY+'px,0px)');
+
+            })
+            
         
         }
-
-
-
-        /*
-        if(calcularScrollProgress(e)>0 && calcularScrollProgress(e)<1){
-            console.log("estoy en seccion:" + idSection)
-            if
-        }*/
     
     });
 
