08b221d08cee4ff156779ccfe76f12c25d26c0cf
FranciscoVazquez <franciscovazquez211@gmail.com>
2024-11-19 17:25:14 -0300

    bug nubes resuelto, add 2 spritesheets

--- Entrega4TP/js/index.js
+++ Entrega4TP/js/index.js
@@ -262,9 +262,7 @@ function cambiarPersonaje(ocultar, mostrar){
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
         let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        //let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        //let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let x = `${_depth1}`;//`${_depth3}, ${_depth2}, ${_depth1}`;
+        let x = `${_depth1}`;
         console.log(x);
         elem.style.backgroundPosition = x;
     }
